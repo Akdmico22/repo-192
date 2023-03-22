88d26e8fe83e7e72822d63809a14c813b9e76196
mariano leiva <marianoandresleiva@gmail.com>
2023-03-22 19:41:50 -0300

    añadiendo mis primeros archivos en git

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+console.log ("hola mundo")
+
+for(let i = 1; i <= 10; i = i +1){
+    console.log("hola mundo")
+    console.log("la variable i vale" + i)
+}
+
+const listaDeProductos = ["agua", "pan", "vino"]
+    console.log (listaDeProductos)
